a93885b8c35f529d9b65e7f0c64b5a7418bc52ff
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-10 09:14:46 +0530

    added the controller

--- auth-service/src/main/java/com/company/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/company/authservice/controller/AuthController.java
@@ -3,23 +3,50 @@ package com.company.authservice.controller;
 
 import com.company.authservice.dto.LoginRequestDto;
 import com.company.authservice.dto.LoginResponseDto;
+import com.company.authservice.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
 public class AuthController {
 
-    @Operation(summary = "Generate token on user Login")
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Operation(summary = "Generate token on user login")
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> login(
-            @RequestBody LoginRequestDto loginRequestDto){
+            @RequestBody LoginRequestDto loginRequestDTO) {
 
-        Optional<String> tokenOptional = authService.authenticate(loginRequestDto);
+        Optional<String> tokenOptional = authService.authenticate(loginRequestDTO);
 
+        if (tokenOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        String token = tokenOptional.get();
+        return ResponseEntity.ok(new LoginResponseDto(token));
+    }
+
+    @Operation(summary = "Validate Token")
+    @GetMapping("/validate")
+    public ResponseEntity<Void> validateToken(
+            @RequestHeader("Authorization") String authHeader) {
+
+        // Authorization: Bearer <token>
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        return authService.validateToken(authHeader.substring(7))
+                ? ResponseEntity.ok().build()
+                : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
--- auth-service/src/main/java/com/company/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/company/authservice/service/AuthService.java
@@ -1,4 +1,45 @@
 package com.company.authservice.service;
 
+
+import com.company.authservice.dto.LoginRequestDto;
+import com.company.authservice.utils.JwtUtil;
+import io.jsonwebtoken.JwtException;
+import java.util.Optional;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
 public class AuthService {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder,
+                       JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
+    public Optional<String> authenticate(LoginRequestDto loginRequestDTO) {
+        Optional<String> token = userService.findByEmail(loginRequestDTO.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(),
+                        u.getPassword()))
+                .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
+
+        return token;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            jwtUtil.validateToken(token);
+            return true;
+        } catch (JwtException e){
+            return false;
+        }
+    }
+
 }
